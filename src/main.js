92423870abed0f1ab89179f5f4fd9ed4f2df5f4f
Brian M. Jemilo II <jemiloii@jemiloii.com>
2013-12-15 22:08:48 -0600

    #landing bgm stops when user leaves page
    #landing bgm stops when user leaves page and plays when user returns to
    the page.

--- src/main.js
+++ src/main.js
@@ -3,18 +3,20 @@ $(function(){
         event.preventDefault ? event.preventDefault() : event.returnValue = false;
         var target = $(this).attr("href");
         $("html, body").stop().animate({
-                scrollLeft: $(target).offset().left,
+                //scrollLeft: $(target).offset().left,
                 scrollTop: $(target).offset().top
             }, 500);
         if(target == '#landing'){
         	$('rain').delay(500).queue( function(next){ 
-		        $(this).show(); 
+		        $(this).show();
+                player2.playVideo();
 		        next(); 
 		      });
         }else{
         	$('rain').delay(500).queue( function(next){ 
-		        $(this).hide(); 
-		        next(); 
+		        $(this).hide();
+                player2.pauseVideo();
+		        next();
 		      });
         }
         $('.navmain a').removeClass('highlight');
